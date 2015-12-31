92aa02cb6c1aa8626a47c95fd25c193af0f8fc20
Logan Buckley <lbuckley@opinionlab.com>
2015-12-30 21:45:15 -0500

    Add if special form

--- interpreter.hs
+++ interpreter.hs
@@ -1,14 +1,30 @@
 module Interpreter where
 
+import Prelude hiding (pred)
 import Control.Monad.Except
 import Scheme
 
 eval :: Env -> SchemeVal -> IOThrowsError SchemeVal
+-- Primitives
 eval env val@(String _) = return val
 eval env val@(Number _) = return val
 eval env val@(Bool _) = return val
 eval env val@(Nil) = return val
+-- QUOTE special form
 eval env (Pair (Atom "quote") (Pair form Nil)) = return form
+-- IF special form
+eval env (Pair (Atom "if") (Pair pred (Pair conseq (Pair alt Nil)))) = do
+   evaledPred <- (eval env pred)
+   case evaledPred of
+     Bool False -> eval env alt
+     Nil -> eval env alt
+     otherwise -> eval env conseq
+eval env (Pair (Atom "if") (Pair pred (Pair conseq Nil))) = do
+  evaledPred <- (eval env pred)
+  case evaledPred of
+    Bool False -> return Nil
+    Nil -> return Nil
+    otherwise -> eval env conseq
 eval env (Pair (Atom func) args) = do
   f <- getVar env func
   evaledArgs <- mapM (eval env) $ unpackList args
